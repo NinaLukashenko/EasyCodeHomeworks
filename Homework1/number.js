763abf29b7502117f209b62c7aaab3428b6b7bb0
NinaLukashenko <nina.lucashenco@gmail.com>
2018-12-25 16:51:23 +0200

    Part Number is done too!

---
+++ Homework1/number.js
@@ -0,0 +1,21 @@
+//task 1:
+console.log(Math.round(Math.PI*100)/100);
+
+//task 2:
+console.log(Math.max(15, 11, 16, 12, 51, 12, 13, 51));
+console.log(Math.min(15, 11, 16, 12, 51, 12, 13, 51));
+
+//task 3-a:
+console.log(+Math.random().toFixed(2));
+
+//task 3-b; my random number is 1000:
+console.log(Math.round(Math.random()*1000));
+
+//task 4:
+console.log(0.6 + 0.7);
+let sum = 0.6 + 0.7;
+console.log(+sum.toFixed(10));
+
+//task 5:
+let money = '100$';
+console.log(parseInt(money));
