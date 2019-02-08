75e9177e97a73fb683ed3a6c5712b9b69ac71e2d
NinaLukashenko <nina.lucashenco@gmail.com>
2019-02-08 20:05:59 +0200

    Tasks 1, 3 are done

---
+++ ProjectAJAX/js/news.ui.service.js
@@ -0,0 +1,54 @@
+class NewsUI {
+  constructor() {
+    this.newsContainer = document.querySelector('.news-wrap .row');
+  }
+
+/**
+ * addArticle - add an article to the UI
+ * @param {Object} article [description]
+ */
+  addArticle(article) {
+    // console.time();
+    const template = NewsUI.generateArticleTemplate(article);
+    // console.timeEnd();
+    this.newsContainer.insertAdjacentHTML('afterbegin', template);
+  }
+
+/**
+ * clearContainer description - cleans the container for articles
+ * @return {[type]} [description]
+ */
+  clearContainer() {
+    let first = this.newsContainer.firstElementChild;
+    while (first) {
+      this.newsContainer.removeChild(first);
+      first = this.newsContainer.firstElementChild;
+    }
+  }
+
+/**
+ * generateArticleTemplate - creates an article template for adding it to UI
+ * @param  {Object} article [description]
+ * @return {[type]}         [description]
+ */
+  static generateArticleTemplate(article) {
+    return `
+      <div class="col s12 m6">
+          <div class="card">
+              <div class="card-image">
+                  <img src="${article.urlToImage}">
+              </div>
+              <div class="card-content">
+                  <span class="card-title">${article.title || ''}</span>
+
+                  <p>${article.description || ''}</p>
+              </div>
+              <div class="card-action">
+                  <a href="${article.url}" target="_blank">Read more</a>
+              </div>
+          </div>
+      </div>
+    `;
+  }
+}
+
