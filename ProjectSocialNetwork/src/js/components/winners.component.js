cce77fc13bddb5155cf27394e0ae9abcf147b65e
NinaLukashenko <nina.lucashenco@gmail.com>
2019-03-06 01:20:30 +0200

    Method render was improved for Winners Component

--- ProjectSocialNetwork/src/js/components/winners.component.js
+++ ProjectSocialNetwork/src/js/components/winners.component.js
@@ -9,30 +9,48 @@ export class WinnersComponent {
   async beforeRender() {
     this._winnes = await this._winnersService.getWinners(15, 1);
     console.log(this._winnes.winners[0].member_id.images[0].image_basic.url);
-    // console.log(this._winnes.winners);
+    console.log(this._winnes.winners);
   }
-
+ 
   render() {
-    return `
+    document.querySelector('body').insertAdjacentHTML('beforeend', `
       <div class="container">
-        <h2>The Winners!</h2> 
-        <div class="row">
-          <div class="col-3">          
-            <img src="${this._winnes.winners[0].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
-          </div>
-          <div class="col-3">          
-            <img src="${this._winnes.winners[1].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
-          </div>
-          <div class="col-3">          
-            <img src="${this._winnes.winners[2].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
-          </div>
-          <div class="col-3">          
-            <img src="${this._winnes.winners[3].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
-          </div>
+        <div class="row winners">
         </div>
       </div>
-    `;
+    `);
+
+    this._winnes.winners.forEach( (item) => {
+      return document.querySelector('.winners').insertAdjacentHTML('beforeend', `
+      <div class="col-3">          
+        <img src="${item.member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
+      </div>
+    `);})
+
+    return `<h2>The Winners!</h2> `;
   }
+
+  // render() {
+  //   return `
+  //     <div class="container">
+  //       <h2>The Winners!</h2> 
+  //       <div class="row">
+  //         <div class="col-3">          
+  //           <img src="${this._winnes.winners[0].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
+  //         </div>
+  //         <div class="col-3">          
+  //           <img src="${this._winnes.winners[1].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
+  //         </div>
+  //         <div class="col-3">          
+  //           <img src="${this._winnes.winners[2].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
+  //         </div>
+  //         <div class="col-3">          
+  //           <img src="${this._winnes.winners[3].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
+  //         </div>
+  //       </div>
+  //     </div>
+  //   `;
+  // }
 
   afterRender() {
   }
