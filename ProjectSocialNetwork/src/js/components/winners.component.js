61902ae39b565938b32e728e8e1ba988b9126f3a
NinaLukashenko <nina.lucashenco@gmail.com>
2019-03-06 01:59:21 +0200

    rendering winners

--- ProjectSocialNetwork/src/js/components/winners.component.js
+++ ProjectSocialNetwork/src/js/components/winners.component.js
@@ -10,45 +10,44 @@ export class WinnersComponent {
     this._winners = await this._winnersService.getWinners(15, 1);
   }
  
-  render() {
-    document.querySelector('app-container').insertAdjacentHTML('afterend', `
-      <div class="container">
-        <div class="row winners">
-        </div>
-      </div>
-    `);
-
-    this._winners.winners.forEach( (item) => {
-      return document.querySelector('.winners').insertAdjacentHTML('beforeend', `
-      <div class="col-3">          
-        <img src="${item.member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
-      </div>
-    `);})
-
-    return `<h2>The Winners!</h2> `;
-  }
-
   // render() {
-  //   return `
+  //   document.querySelector('app-container').insertAdjacentHTML('afterend', `
   //     <div class="container">
-  //       <h2>The Winners!</h2> 
-  //       <div class="row">
-  //         <div class="col-3">          
-  //           <img src="${this._winners.winners[0].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
-  //         </div>
-  //         <div class="col-3">          
-  //           <img src="${this._winners.winners[1].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
-  //         </div>
-  //         <div class="col-3">          
-  //           <img src="${this._winners.winners[2].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
-  //         </div>
-  //         <div class="col-3">          
-  //           <img src="${this._winners.winners[3].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
-  //         </div>
+  //       <div class="row winners">
   //       </div>
   //     </div>
-  //   `;
+  //   `);
+
+  //   this._winners.winners.forEach( (item) => {
+  //     return document.querySelector('.winners').insertAdjacentHTML('beforeend', `
+  //     <div class="col-3">          
+  //       <img src="${item.member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
+  //     </div>
+  //   `);})
+  //   return `<h2>The Winners!</h2>`
   // }
+
+  render() {
+    return `
+      <div class="container">
+        <h2>The Winners!</h2> 
+        <div class="row">
+          <div class="col-3">          
+            <img src="${this._winners.winners[0].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
+          </div>
+          <div class="col-3">          
+            <img src="${this._winners.winners[1].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
+          </div>
+          <div class="col-3">          
+            <img src="${this._winners.winners[2].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
+          </div>
+          <div class="col-3">          
+            <img src="${this._winners.winners[3].member_id.images[0].image_basic.url}" class="rounded img-fluid"> 
+          </div>
+        </div>
+      </div>
+    `;
+  }
 
   afterRender() {
   }
